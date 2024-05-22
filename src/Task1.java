5e17c77feacd3c070d58d0436704097e674369f3
SergeyLevanovv <ser.lewanow@gmail.com>
2024-05-22 04:43:34 +0400

    add Task1.

---
+++ src/Task1.java
@@ -0,0 +1,17 @@
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Задание: 1");
+        int[] fibonacciArray = new int[15];
+        fibonacciArray[0] = 0;
+        fibonacciArray[1] = 1;
+        for (int i = 2; i < 15; i++) {
+            fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray[i - 2];
+        }
+
+        for (int num : fibonacciArray) {
+            if (num % 2 == 0) {
+                System.out.print(num + "\t");
+            }
+        }
+    }
+}
